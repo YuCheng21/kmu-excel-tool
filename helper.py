281c94242616a38805df35e40398b49c0a4b185c
YuCheng <fyc621@gmail.com>
2022-07-11 18:53:05 +0800

    adjust: - cancel two row of merge - blank dataframe - skip error convert #20220711001

--- helper.py
+++ helper.py
@@ -38,6 +38,8 @@ class Helper:
         flag = None
         for index, row in sheet_content.iterrows():
             if not pd.isna(row.loc['施打藥物']) and not pd.isna(row.loc['藥物種類']) and pd.isna(row.loc['副作用']):
+                if flag is True:
+                    normal.append([drug_type, drug, None, None, start_date])
                 drug = row.loc['藥物種類']
                 drug_type = row.loc['施打藥物']
                 start_date = row.loc['新增日期']
@@ -47,48 +49,53 @@ class Helper:
                 flag = False
         if flag is True:
             normal.append([drug_type, drug, None, None, start_date])
+        if len(normal) < 1:
+            return None
         return pd.DataFrame(normal)
 
     def flatten(self):
         result_difficulty = []
         result_severity = []
-        for index, date in enumerate(self.df_normalization.loc[:, 3].unique()):
+        for index, date in enumerate(self.df_normalization.loc[:, 3]):
             CT_DRUG = {v: None for k, v in codebook.CT_DRUG.items()}
             TT_DRUG = {v: None for k, v in codebook.TT_DRUG.items()}
             HT_DRUG = {v: None for k, v in codebook.HT_DRUG.items()}
             effect_difficulty = {v: None for k, v in codebook.effect.items()}
             effect_severity = {v: None for k, v in codebook.effect.items()}
 
-            mask = self.df_normalization[self.df_normalization.loc[:, 3] == date]
-            if date is None:
-                mask = self.df_normalization[self.df_normalization.loc[:, 3].isnull()]
+            # mask = self.df_normalization[self.df_normalization.loc[:, 3] == date]
+            # if date is None:
+            #     mask = self.df_normalization[self.df_normalization.loc[:, 3].isnull()]
 
-            start_date = None
-            for _, row in mask.iterrows():
-                start_date = row.loc[4]
-                if row.loc[1] is not None:
-                    buffer_drug = row.loc[1].split('、')
-                    for _, item in enumerate(buffer_drug):
-                        if item in codebook.CT_DRUG.values():
-                            CT_DRUG[item] = {v: k for k, v in codebook.CT_DRUG.items()}[item]
-                        if item in codebook.TT_DRUG.values():
-                            TT_DRUG[item] = {v: k for k, v in codebook.TT_DRUG.items()}[item]
-                        if item in codebook.HT_DRUG.values():
-                            HT_DRUG[item] = {v: k for k, v in codebook.HT_DRUG.items()}[item]
+            # start_date = None
+            # for _, row in mask.iterrows():
+            row = self.df_normalization.iloc[index]
+            start_date = row.loc[4]
+            if row.loc[1] is not None:
+                buffer_drug = row.loc[1].split('、')
+                for _, item in enumerate(buffer_drug):
+                    if item in codebook.CT_DRUG.values():
+                        CT_DRUG[item] = {v: k for k, v in codebook.CT_DRUG.items()}[item]
+                    if item in codebook.TT_DRUG.values():
+                        TT_DRUG[item] = {v: k for k, v in codebook.TT_DRUG.items()}[item]
+                    if item in codebook.HT_DRUG.values():
+                        HT_DRUG[item] = {v: k for k, v in codebook.HT_DRUG.items()}[item]
 
-                if type(row.loc[2]) is str:
-                    buffer_effect = row.loc[2].split('、')
-                    for _, item in enumerate(buffer_effect):
-                        key = item.split('(')[0]
-                        difficulty = item.split('(')[1].split(':')[0].split('A')[1]
-                        severity = item.split('(')[1].split(':')[1].split(')')[0].split('B')[1]
-                        # if key not in codebook.effect.values(): continue
-                        if key not in codebook.effect.values():
-                            effect_difficulty['其他'] = difficulty
-                            effect_severity['其他'] = severity
-                            continue
-                        effect_difficulty[key] = difficulty
-                        effect_severity[key] = severity
+            if type(row.loc[2]) is str:
+                buffer_effect = row.loc[2].split('、')
+                for _, item in enumerate(buffer_effect):
+                    key = item.split('(')[0]
+                    if len(item.split('(')) <= 1:
+                        continue
+                    difficulty = item.split('(')[1].split(':')[0].split('A')[1]
+                    severity = item.split('(')[1].split(':')[1].split(')')[0].split('B')[1]
+                    # if key not in codebook.effect.values(): continue
+                    if key not in codebook.effect.values():
+                        effect_difficulty['其他'] = difficulty
+                        effect_severity['其他'] = severity
+                        continue
+                    effect_difficulty[key] = difficulty
+                    effect_severity[key] = severity
             CT_DRUG_str = ','.join(list(filter(None, CT_DRUG.values())))
             TT_DRUG_str = ','.join(list(filter(None, TT_DRUG.values())))
             HT_DRUG_str = ','.join(list(filter(None, HT_DRUG.values())))
@@ -126,8 +133,9 @@ class Helper:
         output_path.parent.mkdir(parents=True, exist_ok=True)
         writer = pd.ExcelWriter(output_path)
         for key, value in self.result.items():
-            for i in range(6, 22):
-                value.iloc[:, i] = pd.to_numeric(value.iloc[:, i])
+            if not value.empty:
+                for i in range(6, 22):
+                    value.iloc[:, i] = pd.to_numeric(value.iloc[:, i])
             value.to_excel(writer, key, index=False)
         writer.save()
         return output_path
@@ -137,7 +145,10 @@ class Helper:
         self.result = {}
         for key, value in self.input_excel.items():
             self.df_normalization = self.normalize(key, value)
-            difficulty, severity = self.flatten()
+            if self.df_normalization is None:
+                difficulty, severity = pd.DataFrame([[]]), pd.DataFrame([[]])
+            else:
+                difficulty, severity = self.flatten()
             self.result[f'{self.case_name}-{self.case_id}-困擾度'] = difficulty
             self.result[f'{self.case_name}-{self.case_id}-嚴重度'] = severity
         output_path = self.export(output_file)
@@ -146,4 +157,4 @@ class Helper:
 
 if __name__ == '__main__':
     helper = Helper()
-    helper.auto_run("D:\Downloads\藥物及副作用數據_1657166339.xls", f"./output/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx")
+    helper.auto_run("./input/all.xls", f"./output/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx")
